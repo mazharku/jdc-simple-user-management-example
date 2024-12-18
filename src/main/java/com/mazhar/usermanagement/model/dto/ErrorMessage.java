949393ed80c1ffb7e72037f5caa24c98a45c5686
mazhar <mazhar1322@cseku.ac.bd>
2024-12-19 01:19:21 +0600

    add error message dto

---
+++ src/main/java/com/mazhar/usermanagement/model/dto/ErrorMessage.java
@@ -0,0 +1,15 @@
+package com.mazhar.usermanagement.model.dto;
+
+
+public class ErrorMessage {
+
+    public String message;
+
+    public ErrorMessage() {
+    }
+
+    public ErrorMessage(String message) {
+        this.message = message;
+    }
+
+}
